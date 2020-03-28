6835dbf1c8ff84200cd6792cf4ff41ed80f4ef37
Abhishek Sunil Tiwari <44199550+Abhishek-st@users.noreply.github.com>
2020-03-28 20:58:24 +0530

    Update database.py
    removed vulnerability 2

--- database.py
+++ database.py
@@ -107,7 +107,6 @@
 # body = "This is an email with attachment sent from Python"
 # sender_email = "anktiw2000@gmail.com"
 # receiver_email = "ankit2632000@gmail.com"  
-# password = "Fall@25404"
 
 # # Create a multipart message and set headers
 # message = MIMEMultipart()
